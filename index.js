0e48f5d2be82db96d6964b5e34d392e7270a5dff
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-15 07:03:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your functions here
+let map= function(arr,callback){
+  let newarr=[];
+  for (let i=0;i<arr.length;i++){
+    newarr.push(callback(arr[i]));
+  }
+  return newarr;
+}
+
+
+let reduce = function(arr,callback,startpoint=0){
+  let tem;
+  if(startpoint){
+    tem=startpoint
+  }
+  else
+  {tem=arr[0]}
+  let i;
+  if(startpoint){
+    i=0;
+  }
+  else{
+    i=1;
+  }
+  for(;i<arr.length;i++){
+    tem=callback(tem,arr[i])
+  }
+  return tem;
+}
